906a12b00997809cd58e74486bf8e6788db3f464
jrhe1213 <jiarong1213@gmail.com>
2022-07-31 00:12:50 -0400

    add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,20 @@
 import React from "react";
 import "./App.css";
-import Menu from "./components/Menu";
-import Nav from "./components/Nav";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-
-      <div className="container-fluid">
-        <div className="row">
-          <Menu />
-
-          <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-            <h2>Section title</h2>
-            <div className="table-responsive">
-              <table className="table table-striped table-sm">
-                <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                    <th scope="col">Header</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>1,001</td>
-                    <td>random</td>
-                    <td>data</td>
-                    <td>placeholder</td>
-                    <td>text</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </main>
-        </div>
-      </div>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
